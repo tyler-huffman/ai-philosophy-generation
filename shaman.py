62d580efb15ae61511805c28ba85efc3830b394b
T <43817374+tyler-huffman@users.noreply.github.com>
2019-11-24 01:23:25 -0500

    Can now pass Shaman the name of a philosopher.

--- shaman.py
+++ shaman.py
@@ -2,8 +2,8 @@
 @author: Tyler Huffman
 @date: November 23, 2019
 @description: The RNN model that takes an author's work in, learns, and then generates
-@TODO: Account for non-english chars. More philosophers then Nietzsche include quotes in other languages so I have to decide
-       to either remove them or include them
+@TODO: Account for non-english chars. More philosophers then Nietzsche include quotes in other languages so I have to figure out how
+       to handle them. If more then one text files exists merge themm ignore all files not ending in ".txt"
 '''
 #Source: www.tensorflow.org/tutorials/text/text_generation
 #Source: https://machinelearningmastery.com/text-generation-lstm-recurrent-neural-networks-python-keras/
@@ -12,11 +12,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 import numpy as np
 import os, time, sys
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import LSTM
-from keras.callbacks import ModelCheckpoint
+
+def loss(labels, logits):
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
 def split_input_target(chunk):
     input_text = chunk[:-1]
@@ -27,14 +25,13 @@ def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
   model = tf.keras.Sequential([
     tf.keras.layers.Embedding(vocab_size, embedding_dim,
                               batch_input_shape=[batch_size, None]),
-    tf.keras.layers.GRU(rnn_units,
+    tf.keras.layers.LSTM(rnn_units,
                         return_sequences=True,
                         stateful=True,
                         recurrent_initializer='glorot_uniform'),
     tf.keras.layers.Dense(vocab_size)
   ])
   return model
-
 
 @tf.function
 def train_step(inp, target):
@@ -47,9 +44,6 @@ def train_step(inp, target):
   optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
   return loss
-
-def loss(labels, logits):
-  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
 def generate_text(model, start_string):
   # Evaluation step (generating text using the learned model)
@@ -67,7 +61,7 @@ def generate_text(model, start_string):
   # Low temperatures results in more predictable text.
   # Higher temperatures results in more surprising text.
   # Experiment to find the best setting.
-  temperature = 1.0
+  temperature = 1.3
 
   # Here batch size == 1
   model.reset_states()
@@ -88,6 +82,9 @@ def generate_text(model, start_string):
 
   return (start_string + ''.join(text_generated))
 
+def loss(labels, logits):
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
 def train_on(philosopher):
   try:
     path_to_phil = "Works\\{}\\".format(philosopher)         #Define where to grab the text files
@@ -95,15 +92,15 @@ def train_on(philosopher):
   except FileNotFoundError:                                  #Catch the case where the program is fed an invalid path
     print("Error: {} is not a valid directory")              #Inform the user of the error and exit the programs
     sys.exit(0)
-  
-  text = open(path_to_file, 'rb', encoding='utf-8').read().lower()    #Open the text files, read the contents, and force them into lowercase
-  vocab = sorted(set(text))                                   #Grab the number of unique characters
+  path_to_file = path_to_phil + "\\" + works[0]
+  text = open(path_to_file, 'r', encoding='utf-8').read().lower()    #Open the text files, read the contents, and force them into lowercase
+  chars = sorted(list(set(text)))                                    #Grab the number of unique characters
 
   print("Length of text: {} characters".format(len(text)))
-  print("{} unique characters".format(len(vocab)))
+  print("{} unique characters".format(len(chars)))
 
-  char2idx = {u:i for i, u in enumerate(vocab)}              #Create a map from unique characters to indices
-  idx2char = np.array(vocab)                                 #Create a map from integers to unique chars
+  char2idx = {u:i for i, u in enumerate(chars)}              #Create a map from unique characters to indices
+  idx2char = np.array(chars)                                 #Create a map from integers to unique chars
   text_as_int = np.array([char2idx[c] for c in text])
 
   print('{')
@@ -132,7 +129,7 @@ def train_on(philosopher):
   dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
 
   # Length of the vocabulary in chars
-  vocab_size = len(vocab)
+  vocab_size = len(chars)
 
   # The embedding dimension
   embedding_dim = 256
@@ -140,15 +137,11 @@ def train_on(philosopher):
   # Number of RNN units
   rnn_units = 1024
 
-  model = Sequential()
-  model.add(LTSM(256,input_shape=()))
   model = build_model(
-    vocab_size = len(vocab),
+    vocab_size = len(chars),
     embedding_dim=embedding_dim,
     rnn_units=rnn_units,
     batch_size=BATCH_SIZE)
-
-  optimizer = tf.keras.optimizers.Adam()
 
   for input_example_batch, target_example_batch in dataset.take(1):
       example_batch_predictions = model(input_example_batch)
@@ -156,11 +149,11 @@ def train_on(philosopher):
 
   model.summary()
 
-  example_batch_loss  = loss(target_example_batch, example_batch_predictions)
+  example_batch_loss = loss(target_example_batch, example_batch_predictions)
   print("Prediction shape: ", example_batch_predictions.shape, " # (batch_size, sequence_length, vocab_size)")
   print("scalar_loss:      ", example_batch_loss.numpy().mean())
 
-  ##model.compile(optimizer='adam', loss=loss)
+  model.compile(optimizer='adam', loss=loss)
 
   checkpoint_dir = './training_checkpoints/{}'.format(philosopher)  #Directory where the checkpoints will be saved
   checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")  #Name of the checkpoint files
@@ -169,38 +162,17 @@ def train_on(philosopher):
       filepath=checkpoint_prefix,
       save_weights_only=True)
 
-  EPOCHS = 10
-  for epoch in range(EPOCHS):
-    start = time.time()
+  EPOCHS = 40
 
-    # initializing the hidden state at the start of every epoch
-    # initally hidden is None
-    hidden = model.reset_states()
-
-    for (batch_n, (inp, target)) in enumerate(dataset):
-      loss = train_step(inp, target)
-
-      if batch_n % 100 == 0:
-        template = 'Epoch {} Batch {} Loss {}'
-        print(template.format(epoch+1, batch_n, loss))
-
-    # saving (checkpoint) the model every 5 epochs
-    if (epoch + 1) % 5 == 0:
-      model.save_weights(checkpoint_prefix.format(epoch=epoch))
-
-    print ('Epoch {} Loss {:.4f}'.format(epoch+1, loss))
-    print ('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
-
-  model.save_weights(checkpoint_prefix.format(epoch=epoch))
-
-  ##history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
-
-  ##tf.train.latest_checkpoint(checkpoint_dir)
-  ##model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
-
-  ##model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
-
-  ##model.build(tf.TensorShape([1, None]))
+  history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+  
+  tf.train.latest_checkpoint(checkpoint_dir) #Train the RNN
+  
+  model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+  model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+  model.build(tf.TensorShape([1, None]))
   model.summary()
 
   print(generate_text(model, start_string=u"Life,  "))
+
+train_on("Nietzsche")
